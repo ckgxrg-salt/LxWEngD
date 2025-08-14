a1aed778b6152bc88bb05a7f9af368afaa914894
ckgxrg <ckgxrg@ckgxrg.io>
2025-08-15 00:28:46 +0800

    playlist: rewrite

--- daemon/src/playlist.rs
+++ daemon/src/playlist.rs
@@ -1,27 +1,17 @@
 //! Finds playlist files in some given search path.
 #![warn(clippy::pedantic)]
 
-use std::collections::BTreeMap;
-use std::error::Error;
-use std::fmt::Display;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
+use thiserror::Error;
 
+use crate::cli::SEARCH_PATH;
 use crate::commands::{Command, identify};
 
-#[derive(Debug, PartialEq)]
-pub enum PlaylistError {
-    FileNotFound,
-}
-impl Error for PlaylistError {}
-impl Display for PlaylistError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            PlaylistError::FileNotFound => write!(f, "Cannot find the playlist file"),
-        }
-    }
-}
+#[derive(Debug, PartialEq, Error)]
+#[error("cannot find playlist file `{0}`")]
+pub struct FileNotFound(PathBuf);
 
 /// Searches the given playlist in the given search path.
 ///
@@ -31,39 +21,38 @@ impl Display for PlaylistError {
 /// Finally, tries to find a file relative to `search_path` with name `filename.playlist`.
 ///
 /// # Errors
-/// If none of these approaches can find a playlist file, returns `PlaylistError::FileNotFound`.
-pub fn find(filename: &Path, search_path: &Path) -> Result<File, PlaylistError> {
+/// If none of these approaches can find a playlist file, returns `FileNotFound`.
+pub fn open(filename: &Path) -> Result<File, FileNotFound> {
     // Fully qualified path
     if filename.is_file() {
-        return Ok(File::open(filename).map_err(|_| PlaylistError::FileNotFound))?;
+        return Ok(File::open(filename).map_err(|_| FileNotFound(filename.to_path_buf())))?;
     }
 
     // Relative to default with extension
-    let mut real_path = search_path.to_path_buf();
+    let mut real_path = SEARCH_PATH.to_path_buf();
     real_path.push(filename);
     if real_path.is_file() {
-        return Ok(File::open(real_path).map_err(|_| PlaylistError::FileNotFound))?;
+        return Ok(File::open(&real_path).map_err(|_| FileNotFound(real_path)))?;
     }
 
     // Relative to default without extension
-    let mut real_path = search_path.to_path_buf();
+    let mut real_path = SEARCH_PATH.to_path_buf();
     real_path.push(filename);
     let mut temp = real_path.into_os_string();
     temp.push(".playlist");
     let real_path = PathBuf::from(temp);
     if real_path.is_file() {
-        return Ok(File::open(real_path).map_err(|_| PlaylistError::FileNotFound))?;
+        return Ok(File::open(&real_path).map_err(|_| FileNotFound(real_path)))?;
     }
 
-    Err(PlaylistError::FileNotFound)
+    Err(FileNotFound(filename.to_path_buf()))
 }
 
 /// Parses a playlist file and generates a list of [`Command`]
 /// This process will load the playlist file into memory, parse it, and generate a list of
 /// [`Command`].
-#[must_use]
-pub fn parse(path: &Path, file: &File) -> BTreeMap<usize, Command> {
-    let mut commands = BTreeMap::new();
+pub fn parse(path: &Path, file: &File) -> Vec<Command> {
+    let mut commands = Vec::new();
     let lines: Vec<String> = BufReader::new(file)
         .lines()
         .enumerate()
@@ -88,7 +77,7 @@ pub fn parse(path: &Path, file: &File) -> BTreeMap<usize, Command> {
         };
         match identify(each) {
             Ok(cmd) => {
-                commands.insert(num, cmd);
+                commands.push(cmd);
             }
             Err(err) => {
                 log::warn!(
@@ -114,18 +103,15 @@ mod tests {
     fn find_playlist() {
         let mut content: String = String::from("");
         // Fully qualified path
-        find(
-            &PathBuf::from("./playlists/open_test.playlist"),
-            &PathBuf::from("Nothing"),
-        )
-        .unwrap()
-        .read_to_string(&mut content)
-        .unwrap();
+        open(&PathBuf::from("./playlists/open_test.playlist"))
+            .unwrap()
+            .read_to_string(&mut content)
+            .unwrap();
         assert_eq!(content, "=)\n");
 
         // No extension
         content.clear();
-        find(&PathBuf::from("open_test"), &PathBuf::from("playlists"))
+        open(&PathBuf::from("open_test"))
             .unwrap()
             .read_to_string(&mut content)
             .unwrap();
@@ -133,40 +119,25 @@ mod tests {
 
         // With extension
         content.clear();
-        find(
-            &PathBuf::from("open_test.playlist"),
-            &PathBuf::from("playlists"),
-        )
-        .unwrap()
-        .read_to_string(&mut content)
-        .unwrap();
+        open(&PathBuf::from("open_test.playlist"))
+            .unwrap()
+            .read_to_string(&mut content)
+            .unwrap();
         assert_eq!(content, "=)\n");
     }
 
     #[test]
     fn parse_playlist() {
         let playlist = PathBuf::from("./playlists/default.playlist");
-        let commands = parse(
-            &playlist,
-            &find(&playlist, &PathBuf::from("Nothing")).unwrap(),
-        );
+        let commands = parse(&playlist, &open(&playlist).unwrap());
 
-        let mut expected = BTreeMap::new();
-        expected.insert(
-            0,
+        let expected = vec![
             Command::Wallpaper(1, Duration::from_secs(15 * 60), false, HashMap::new()),
-        );
-        expected.insert(
-            1,
             Command::Wallpaper(2, Duration::from_secs(1 * 60 * 60), false, HashMap::new()),
-        );
-        expected.insert(
-            2,
             Command::Wallpaper(3, Duration::from_secs(360), false, HashMap::new()),
-        );
-        expected.insert(4, Command::Wait(Duration::from_secs(5 * 60)));
-        expected.insert(5, Command::End);
-
+            Command::Wait(Duration::from_secs(5 * 60)),
+            Command::End,
+        ];
         assert_eq!(commands, expected);
     }
 }
